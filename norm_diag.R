94e6694f00531a84bb79d32b209367a72392c5f3
gabriel stein <steingps@gmail.com>
2019-06-20 20:31:13 -0300

    corrigi a função para não cortar os nomes

--- norm_diag.R
+++ norm_diag.R
@@ -3,25 +3,26 @@ library(qqplotr)
 library(gridExtra)
 
 normal_diag <- function(fit.model){
+  alt_theme <- theme(text = element_text(size=10))
   student_residuals <- tibble(residual = rstudent(fit.model),
                               fit = fitted(fit.model),
                               index = names(rstudent(fit.model)) %>% as.numeric())
   residual_index <- student_residuals %>% ggplot(aes(x = index, y = residual)) +
     geom_point() + geom_hline(yintercept = c(-2,0,2),linetype = "dashed") +
-    labs(x="Índice", y="Resíduo Studentizado")
+    labs(x="Índice", y="Resíduo Studentizado") + alt_theme
   residual_fitted <- student_residuals %>% ggplot(aes(x = fit, y = residual)) +
     geom_point() + geom_hline(yintercept = c(-2,0,2),linetype = "dashed") +
-    labs(x="Valores Ajustados", y="Resíduo Studentizado")
+    labs(x="Valores Ajustados", y="Resíduo Studentizado") + alt_theme
   histbase <- hist(student_residuals$residual, plot = FALSE)
   residual_histogram <- student_residuals %>% ggplot(aes(x = residual)) +
     geom_histogram(aes(y = ..density..),
                    bins = histbase$breaks %>% length(),
                    breaks=histbase$breaks,
                    color="black", fill="white",center=0) +
-    labs(y="Densidade", y="Resíduo Studentizado")
+    labs(y="Densidade", y="Resíduo Studentizado") + alt_theme
   residual_qqplot <- student_residuals %>% ggplot(aes(sample = residual)) +
     stat_qq_band(bandType = "boot", alpha = 0.4) +
     stat_qq_line(colour = "#8DA0CB") +
-    stat_qq_point() + labs(y ="Resíduo Studentizado", x = "Percentil da N(0,1)")
+    stat_qq_point() + labs(y ="Resíduo Studentizado", x = "Percentil da N(0,1)") + alt_theme
   grid.arrange(residual_index, residual_fitted, residual_histogram, residual_qqplot)
 }
